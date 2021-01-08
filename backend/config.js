859e2487cb0e76e561710a5eae9bfe5656a673d0
Aditya Harish <adityaharish@Adityas-MacBook-Pro.local>
2021-01-08 14:49:05 +0530

    final commit

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/ecommerce',
+  PORT: process.env.PORT || 5000,
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
-}
+  
+};
